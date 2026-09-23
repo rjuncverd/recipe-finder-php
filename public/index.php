846b080fc38dfb3613b7628c01b6b8cd04d514b1
Ruben <rubenjuncalverde@gmail.com>
2026-09-24 00:35:37 +0200

    Se añade Swagger a la API

--- public/index.php
+++ public/index.php
@@ -8,12 +8,14 @@ $uri = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?? '/';
 
 if ($uri === '/' || $uri === '') {
     echo json_encode([
-        'message' => 'API disponible. Usa GET /recipes?name=...'
+        'message' => 'API disponible. Usa GET /recipes?name=...',
+        'swagger' => '/swagger.html'
     ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
     exit;
 }
 
 http_response_code(404);
 echo json_encode([
-    'error' => 'Route not found. Use GET /recipes?name=...'
+    'error' => 'Route not found. Use GET /recipes?name=...',
+    'swagger' => '/swagger.html'
 ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
